10b7d947a7ddea02afe577e825469d436901a3a5
seiko <seiko_des@outlook.com>
2022-05-04 09:54:39 +0800

    use collectLast for Flow.observer()

--- app/src/main/java/com/seiko/lightnovel/util/FlowExt.kt
+++ app/src/main/java/com/seiko/lightnovel/util/FlowExt.kt
@@ -1,14 +1,24 @@
 package com.seiko.lightnovel.util
 
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 context(LifecycleOwner)
-fun <T> Flow<T>.observer(action: suspend (T) -> Unit): Job {
-    return flowWithLifecycle(lifecycle).onEach(action).launchIn(lifecycleScope)
+fun <T> Flow<T>.observer(
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    action: suspend (T) -> Unit
+): Job {
+    return lifecycleScope.launch {
+        repeatOnLifecycle(minActiveState) {
+            collectLatest {
+                action(it)
+            }
+        }
+    }
 }
